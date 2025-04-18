5646e2d4a1454a54f3c094b75c6bb16bd3e9eb45
wally-w4i <walking4icecream@gmail.com>
2025-04-18 20:09:50 -0300

    fix baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://wally-w4i.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/elpencalito/',
+  baseUrl: '/elpencalito-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
